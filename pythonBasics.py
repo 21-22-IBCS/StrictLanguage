05d851eb01b4f8027c4a410bc49269f8ef7b4e07
Zichong Chen <zichong_chen@m018289.local>
2021-09-21 15:12:03 -0700

    Homework

---
+++ pythonBasics.py
@@ -0,0 +1,25 @@
+def main():
+    #2) Name of the assignment and the date
+    print("Python Basic Commands Practice")
+    print("Date: 09/21/2021")
+    
+    #3) Sum of two integers
+    a = 5
+    b = 19
+    print("5 + 19 = " + str(a+b))
+    
+    #4) Product of two intergers
+    a = 5
+    b = 19
+    print("5 * 19 = " + str(a*b))
+    
+    #5) Current students number
+    numStudents = 20
+    print("There are currently " + str(numStudents) + " number of students in the USB")
+    numStudents = 80
+    print("There are currently " + str(numStudents) + " number of students in the USB")
+    numStudents = 114
+    print("There are currently " + str(numStudents) + " number of students in the USB")
+    
+if __name__ == "__main__":
+    main()
